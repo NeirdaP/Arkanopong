8cec041894f98c2d3404e5d6e210b9f22d564db4
NeirdaP <adripatriz@gmail.com>
2017-04-26 14:54:07 +0200

    Add the game/player/ball/brick struct files

--- dist/src/main.c
+++ dist/src/main.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <OpenGL/gl.h>
-#include <OpenGL/glu.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
 #include <SDL/SDL.h>
 
 
@@ -15,32 +15,49 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Impossible d'initialiser la SDL. Fin du programme.\n");
         return EXIT_FAILURE;
     }
-    
+
     SDL_Surface* screen = NULL;
-    if(NULL == (screen = SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, 
+    if(NULL == (screen = SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL,
         SDL_DOUBLEBUF | SDL_OPENGL))) {
         fprintf(stderr, "Impossible d'ouvrir la fenetre. Fin du programme.\n");
         return EXIT_FAILURE;
     }
     SDL_WM_SetCaption("Arkanopong", NULL);
-    
-    
+
+
     int loop = 1;
     while(loop) {
-        
+
         SDL_GL_SwapBuffers();
-        
-    
+
         SDL_Event e;
         while(SDL_PollEvent(&e)) {
+            /*Click on the window's top cross*/
             if(e.type == SDL_QUIT) {
-            loop = 0;
-            break;
-        }
+              loop = 0;
+              break;
+            }
+            /*Keypress on keyboard*/
+            switch(e.type) {
+              case SDL_KEYDOWN:
+            	  switch(e.key.keysym.sym){
+            	    case 'q' :
+            	    case SDLK_ESCAPE :
+              		    loop = 0;
+              		    break;
+            	    default :
+                      printf("%c\n", e.key.keysym.sym);
+                  break;
+            	  }
+                break;
+
+              default:
+                break;
+            }
         }
     }
-  
+
     SDL_Quit();
-    
+
     return EXIT_SUCCESS;
 }
